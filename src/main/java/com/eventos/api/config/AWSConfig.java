f1415b00e4cd5114352d10ce5f5f1127267e9df1
Jose Carlos <48320837+Jebyto@users.noreply.github.com>
2026-09-02 20:15:58 -0300

    chore(storage): configure AWS S3 client

---
+++ src/main/java/com/eventos/api/config/AWSConfig.java
@@ -0,0 +1,25 @@
+package com.eventos.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class AWSConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public S3Client createS3Instance() {
+
+        return S3Client.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(DefaultCredentialsProvider.builder().build())
+                .build();
+    }
+}
